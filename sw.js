21428f762b40aaf6f0dc5ff74676163fac7f1662
jjwall101-lgtm <jjwall101@hotmail.co.uk>
2026-06-18 21:20:57 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener("notificationclick", event => {
-  event.notification.close();
-  const url = event.notification?.data?.url || "./index.html";
-  event.waitUntil(clients.openWindow(url));
-});
